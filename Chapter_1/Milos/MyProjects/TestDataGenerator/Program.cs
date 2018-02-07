d4a2e0ab168160a886789fd8782ca8776cba5e03
milosstojanovic <milos.stojanovic@mocosha.com>
2018-02-08 00:13:40 +0100

    Added "Milos" directory. Added project for generating test data.

---
+++ Chapter_1/Milos/MyProjects/TestDataGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = Generator.GenerateNumbers();
+
+            Console.WriteLine(string.Join(" ", data));
+            Console.ReadKey();
+        }
+    }
+}
