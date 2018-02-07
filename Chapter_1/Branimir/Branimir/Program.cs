daeffe56a0f3d06cb5fbc78b767ab0f874bc8419
Branimir <branimir.momcilovic@gmail.com>
2018-02-07 18:10:28 +0100

    Framework updated to 4.7
    int[] -> IEnumerable<int>

---
+++ Chapter_1/Branimir/Branimir/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branimir
+{
+    class Program
+    {
+        public delegate int MaxDelegate(int[] data);
+
+        private static int[] data = Enumerable.Range(0,99999).ToArray();
+
+        private static void Benchmark(MaxDelegate myMethod, IEnumerable<int> data)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            var result = myMethod.DynamicInvoke(data);
+            sw.Stop();
+            Console.WriteLine("Time taken: {0}ms, result: {1}", sw.Elapsed.TotalMilliseconds, result);
+        }
+
+        private static int FrameworkMax(int[] data)
+        {
+            return data.Max();
+        }
+
+        static void Main(string[] args)
+        {
+            MaxDelegate myMethod = FrameworkMax;
+            Benchmark(myMethod, data);
+
+            Console.ReadLine();
+        }
+    }
+}
