dec16ab06069449fa293c239f868922a2e517b6c
Branimir <branimir.momcilovic@gmail.com>
2018-02-08 09:37:59 +0100

    csproj added

---
+++ Chapter_1/Branimir/Branimir/Maxx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branimir
+{
+    class Maxx
+    {
+        public Maxx(IEnumerable<int> data)
+        {
+            var max = int.MinValue;
+
+            foreach (var item in data)
+                max = max > item ? max : item;
+        }
+    }
+}
