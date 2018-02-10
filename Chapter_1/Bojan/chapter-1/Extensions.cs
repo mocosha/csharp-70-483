5ecd26e0555f8dacc735c945b5e93af949ed8365
Bojan Pantovic <bojan.pantovic@gmail.com>
2018-02-10 17:12:09 +0100

    Try to implement MaxTaks and MaxPlinq

---
+++ Chapter_1/Bojan/chapter-1/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bojan
+{
+    public static class Extensions
+    {
+        public static int MyMaxx(this int[] data)
+        {
+            var max = int.MinValue;
+
+            foreach (var item in data)
+                max = max > item ? max : item;
+
+            return max;
+        }
+
+        public static int[] SubArray(this int[] data, int index, int length)
+        {
+            int[] result = new int[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+    }
+}
