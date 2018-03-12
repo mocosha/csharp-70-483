e8c849ff60371826fa25da232785b51afa440224
Branimir <branimir.momcilovic@gmail.com>
2018-03-12 15:01:43 +0100

    DataStructures project added, Stack implemented

---
+++ Chapter_2/DataStructures/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{   
+
+    class Program
+    {
+        static void StackUsageSample()
+        {
+            var stack = new Stack<int>();
+
+            Console.WriteLine("Empty stack count: {0}", stack.Length);
+
+            stack.Push(1);
+            Console.WriteLine("1 pushed to the stack, count: {0}", stack.Length);
+            stack.Push(54);
+            stack.Push(13);
+
+            Console.WriteLine("Count: {0}", stack.Length);
+
+            Console.Write("Pop all elements:");
+
+            while (stack.Length > 0)
+                Console.Write("{0} ", stack.Pop());
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            StackUsageSample();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter_2/DataStructures/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    /// <summary>
+    /// Educational purpose only stack implementation
+    /// Interface https://msdn.microsoft.com/en-us/library/bb339909(v=vs.110).aspx?
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Stack<T>
+    {
+        public Stack() : this(100) { }
+
+        public Stack(int capacity)
+        {
+            _items = new T[capacity];
+        }
+
+        public void Push(T item)
+        {
+            if (_currentIndex >= _items.Length)
+            { throw new StackOverflowException(); }
+
+            _items[_currentIndex++] = item;
+        }
+
+        public T Pop()
+        {
+            if (_currentIndex <= 0)
+            { throw new Exception("Stack is empty"); }
+
+            return _items[--_currentIndex];
+        }
+
+        public int Length { get { return _currentIndex; } }
+
+        private T[] _items = null;
+
+        private int _currentIndex = 0;
+    }
+}
