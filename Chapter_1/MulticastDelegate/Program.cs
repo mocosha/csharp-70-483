7731b6e0a926ab29ba7ef2a36463c9bb512fcdde
Bojan Pantovic <bojan.pantovic@gmail.com>
2018-03-15 23:45:00 +0100

    Multicast delegate examples added

--- Chapter_1/MulticastDelegate/Program.cs
+++ Chapter_1/MulticastDelegate/Program.cs
@@ -1,62 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Bojan
+namespace MulticastDelegate
 {
     class Animal { }
-    class Dogs : Animal { }
+    class Dog : Animal { }
 
     class Program
     {
         public delegate void SimpleDel(string str);
 
-        
-        public delegate Animal HandlerMethod();
-
-        public static Animal AnimalHandler()
-        {
-            return null;
-        }
-
-        public static Dogs DogsHandler()
-        {
-            return null;
-        }
-
         public static void Hello(string msg)
         {
-            Console.WriteLine("Hello, {0}!", msg);
+            Console.WriteLine("\tHello, {0}!", msg);
         }
 
         public static void Goodbye(string msg)
         {
-            Console.WriteLine("Goodbye, {0}!", msg);
+            Console.WriteLine("\tGoodbye, {0}!", msg);
         }
 
-        static void Main(string[] args)
+        static void MulticastDelegatExample()
         {
-            //MULTICAST DELEGAT
-            //https://docs.microsoft.com/en-us/dotnet/api/system.multicastdelegate?view=netframework-4.7.1
-            //SimpleDel del = Hello;
-            //del += Goodbye;
-            //SimpleDel hello = Hello;
-            //SimpleDel goodbye = Goodbye;
-            //SimpleDel del = (SimpleDel)Delegate.Combine(hello, goodbye);
-            //ShowAllMethods(del);
-            //del("Bojan");
+            SimpleDel hello = Hello;
+            SimpleDel goodbye = Goodbye;
+            SimpleDel multicast1 = hello + goodbye;
+            SimpleDel multicast2 = (SimpleDel)Delegate.Combine(hello, goodbye);
 
-            //TODO: get all results?
+            Console.WriteLine("Invoking hello delegat");
+            hello("Bojan");
+            Console.WriteLine("Invoking goodbye delegat");
+            goodbye("Bojan");
 
-            //Covariance and Contravariance 
-            //http://www.tutorialsteacher.com/csharp/csharp-covariance-and-contravariance
-            HandlerMethod handlerAnimal = AnimalHandler;
-            // Covariance enables this assignment.  
-            HandlerMethod handlerDogs = DogsHandler;
+            Console.WriteLine("Invoking multicast1 delegat");
+            multicast1("Bojan");
 
-            Console.ReadKey();
+            Console.WriteLine("Invoking multicast2 delegat");
+            multicast2("Bojan");
+        }
+
+        public delegate Animal VarianceDel(Dog dog);
+
+        public static Dog Method1(Dog dog)
+        {
+            Console.WriteLine("Method1");
+            return new Dog();
+        }
+
+        public static Animal Method2(Dog dog)
+        {
+            Console.WriteLine("Method2");
+            return new Animal();
+        }
+
+        public static Animal Method3(Animal animal)
+        {
+            Console.WriteLine("Method3");
+            return new Animal();
+        }
+
+        static void CovarianceExample()
+        {
+            Console.WriteLine("Covariance Example");
+            VarianceDel del = Method1;
+            del += Method2;
+            //Thus, covariance allows you to assign a method to the delegate that has a less derived return type.
+            Animal animal = del(new Dog());
+        }
+
+        static void ContravarianceExample()
+        {
+            Console.WriteLine("Contravariance Example");
+            VarianceDel del = Method1;
+            del += Method2;
+            //Method3 has a parameter of Animal class whereas delegate expects a parameter of Dog class. 
+            del += Method3;
+            Animal animal = del(new Dog());
+        }
+
+        static void ShowAllDelegatMethodsExample()
+        {
+            Console.WriteLine("Show all delegat methods example");
+            SimpleDel hello = Hello;
+            SimpleDel goodbye = Goodbye;
+            SimpleDel multicast1 = hello + goodbye;
+
+            ShowAllMethods(multicast1);
         }
 
         private static void ShowAllMethods(Delegate del)
@@ -67,6 +95,16 @@ namespace Bojan
                 Console.WriteLine("Method {0}: {1}", i, item.Method.Name);
                 i++;
             }
+        }
+
+        static void Main(string[] args)
+        {
+            //MulticastDelegatExample();
+            //CovarianceExample();
+            //ContravarianceExample();
+            ShowAllDelegatMethodsExample();
+
+            Console.ReadKey();
         }
     }
 }
