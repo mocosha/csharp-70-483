eeb583482ba1d7cfb38ccb8f3991c331dbe21c2c
Branimir <branimir.momcilovic@gmail.com>
2018-03-23 17:40:00 +0100

    Namespace and assembly name update to DataStructures

--- Chapter_2/DataStructures/Stack.cs
+++ Chapter_2/DataStructures/Stack.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Stack
+namespace DataStructures
 {
     /// <summary>
     /// Educational purpose only stack implementation
