eba48cd45b061bb8bb31dc6b58f64ac11dfcb8d5
Bojan Pantovic <bojan.pantovic@gmail.com>
2018-04-17 01:03:36 +0200

    FluentInterface pattern

---
+++ Chapter_2/DesignPatterns/FluentInterface.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    public class Reservation
+    {
+        public string ConfirmationNumber { get; set; }
+        public DateTime PickupDateTime { get; set; }
+        public string Pickup { get; set; }
+        public string DropOff { get; set; }
+        public string Pax { get; set; }
+
+        public override string ToString()
+        {
+            return $"Reservation #{ConfirmationNumber} at {PickupDateTime}\n\tPickup: {Pickup}\n\tDropoff: {DropOff}\n\tPax: {Pax}";
+        }
+    }
+
+    public class ReservationBuilder
+    {
+        private Reservation _reservation = new Reservation();
+        
+        public ReservationBuilder CreateNew(string confirmationNumber)
+        {
+            _reservation.ConfirmationNumber = confirmationNumber;
+            return this;
+        }
+
+        public ReservationBuilder WithPickupDateTime(DateTime pickupDateTime)
+        {
+            _reservation.PickupDateTime = pickupDateTime;
+            return this;
+        }
+
+        public ReservationBuilder WithPickup(string pickup)
+        {
+            _reservation.Pickup = pickup;
+            return this;
+        }
+
+        public ReservationBuilder WithDropoff(string dropoff)
+        {
+            _reservation.DropOff = dropoff;
+            return this;
+        }
+
+        public ReservationBuilder WithPax(string pax)
+        {
+            _reservation.Pax = pax;
+            return this;
+        }
+
+        public static implicit operator Reservation(ReservationBuilder rb)
+        {
+            return rb._reservation;
+        }
+
+    }
+}
--- Chapter_2/DesignPatterns/Program.cs
+++ Chapter_2/DesignPatterns/Program.cs
@@ -13,10 +13,25 @@ namespace DesignPatterns
             Singleton.Instance.Log("Singeton sample method started");
         }
 
+        static void FluentInterfaceSample()
+        {
+            ReservationBuilder rb = new ReservationBuilder();
+
+            Reservation reservation = rb.CreateNew("17001212")
+                .WithPickupDateTime(DateTime.Now.AddDays(2))
+                .WithPickup("Milutina Milankovica 22")
+                .WithDropoff("Jurija Gagarina 12")
+                .WithPax("Bojan Pantovic");
+
+            Console.WriteLine(reservation);
+        }
+
         static void Main(string[] args)
         {
-            SingletonSample();
-            Singleton.Instance.Log("test entry");;
+            //SingletonSample();
+            //Singleton.Instance.Log("test entry");;
+
+            FluentInterfaceSample();
 
             Console.ReadLine();
         }
