b4ff65a0ffbfc1dce3106849994e0aef59825371
Bojan Pantovic <bojan.pantovic@gmail.com>
2018-09-18 00:07:55 +0200

    AssemblyReader: Made it as a tool + refactoring

--- Chapter_3/AssemblyReader/Program.cs
+++ Chapter_3/AssemblyReader/Program.cs
@@ -11,24 +11,18 @@ namespace AssemblyReader
     /// </summary>
     class Program
     {
-        static bool IsOptionAssembly(string option)
+        static void ShowUsage()
         {
-            return string.Equals(option, "assembly", StringComparison.OrdinalIgnoreCase);
-        }
+            Console.WriteLine("Usage: version [options] [path]");
 
-        static bool IsOptionProduct(string option)
-        {
-            return string.Equals(option, "product", StringComparison.OrdinalIgnoreCase);
-        }
+            Console.WriteLine("Options:");
+            Console.WriteLine("\t --a | --assembly");
+            Console.WriteLine("\t --p | --product");
+            Console.WriteLine("\t --f | --file");
+            Console.WriteLine("\t --all");
 
-        static bool IsOptionFile(string option)
-        {
-            return string.Equals(option, "file", StringComparison.OrdinalIgnoreCase);
-        }
-
-        static bool IsOptionAll(string option)
-        {
-            return string.Equals(option, "all", StringComparison.OrdinalIgnoreCase);
+            Console.WriteLine("Path:");
+            Console.WriteLine("\t The path to the assembly");
         }
 
         const int MAX_PATH = 256;
@@ -36,17 +30,7 @@ namespace AssemblyReader
         {
             if (args.Length != 2)
             {
-                Console.WriteLine("Usage: version [options] [path]");
-
-                Console.WriteLine("Options:");
-                Console.WriteLine("\t assembly");
-                Console.WriteLine("\t product");
-                Console.WriteLine("\t file");
-                Console.WriteLine("\t all");
-
-                Console.WriteLine("Path:");
-                Console.WriteLine("\t The path to the assembly");
-
+                ShowUsage();
                 return;
             }
 
@@ -58,39 +42,41 @@ namespace AssemblyReader
             //.exe and .dll
             if (string.IsNullOrWhiteSpace(assemblyPath))
             {
-
+                Console.WriteLine($"Assembly path is missing");
+                return;
             }
 
             try
             {
                 var assembly = Assembly.LoadFrom(assemblyPath);
-
-                if (assembly.FullName.Length > Program.MAX_PATH)
-                {
-                    Console.WriteLine("");
-                    return;
-                }
-
                 //TODO: check this methods
                 //var assembly = Assembly.LoadFile(assemblyPath);
 
                 //var an = AssemblyName.GetAssemblyName(assemblyPath);
                 //var assembly = Assembly.Load(an);
 
+                if (assembly.FullName.Length > Program.MAX_PATH)
+                {
+                    Console.WriteLine($"Max length for namespace exceeded max value of {Program.MAX_PATH}");
+                    return;
+                }
+
                 if (IsOptionAll(option) || IsOptionAssembly(option))
                 {
                     string assemblyVersion = assembly.GetName().Version.ToString();
                     Console.WriteLine($"Assembly version: {assemblyVersion}");
                 }
-                else if (IsOptionAll(option) || IsOptionProduct(option))
+
+                if (IsOptionAll(option) || IsOptionProduct(option))
                 {
-                    string productVersion = FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
-                    Console.WriteLine($"Product version: {productVersion}");
+                    var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+                    Console.WriteLine($"Product version: {versionInfo.ProductVersion}");
                 }
-                else if (IsOptionAll(option) || IsOptionFile(option))
+
+                if (IsOptionAll(option) || IsOptionFile(option))
                 {
-                    string fileVersion = FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
-                    Console.WriteLine($"File version: {fileVersion}");
+                    var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+                    Console.WriteLine($"File version: {versionInfo.FileVersion}");
                 }
             }
             catch (FileNotFoundException ex)
@@ -105,7 +91,7 @@ namespace AssemblyReader
                 Debug.WriteLine(ex.GetType().FullName);
                 Debug.WriteLine(ex.StackTrace);
 
-                Console.WriteLine("A file that was found could not be loaded."); 
+                Console.WriteLine("A file that was found could not be loaded.");
             }
             catch (BadImageFormatException ex)
             {
@@ -129,10 +115,39 @@ namespace AssemblyReader
             //    Console.WriteLine(ex.Message);
             //}
 
-#if DEBUG
             Console.ReadKey(true);
-#endif
+        }
 
+        private static bool IsOptionAssembly(string option)
+        {
+            var values = new string[] { "--assembly", "--a" };
+            return Array.Exists(values, v => v == option);
+
+            //return string.Equals(option, "--assembly", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static bool IsOptionProduct(string option)
+        {
+            var values = new string[] { "--product", "--p" };
+            return Array.Exists(values, v => v == option);
+
+            //return string.Equals(option, "--product", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static bool IsOptionFile(string option)
+        {
+            var values = new string[] { "--file", "--f" };
+            return Array.Exists(values, v => v == option);
+
+            //return string.Equals(option, "--file", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static bool IsOptionAll(string option)
+        {
+            var values = new string[] { "--all" };
+            return Array.Exists(values, v => v == option);
+
+            //return string.Equals(option, "--all", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
