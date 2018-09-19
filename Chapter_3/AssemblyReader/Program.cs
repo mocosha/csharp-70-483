84948c11c87b5ec4f4cfcfaa217225e567b0420a
Bojan Pantovic <bojan.pantovic@gmail.com>
2018-09-19 23:23:55 +0200

    AssemblyReader: minor improvements and refactoring

--- Chapter_3/AssemblyReader/Program.cs
+++ Chapter_3/AssemblyReader/Program.cs
@@ -1,24 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Security;
-/*
-TODO:
-    first param file path, second option with default (Company name, Assmbly version, File version) --all (+ product version...)
-
-    use library for parsing arguments
-    validate allowed arguments
-    validate file type (.exe and .dll)
-
-    target framework
-    company name
-
-    return error codes
-    branding
-
-    read assembly informations and put to dict
- */
 
 namespace AssemblyReader
 {
@@ -30,15 +15,15 @@ namespace AssemblyReader
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            var argsLen = args.Length;
+            if (argsLen > 2)
             {
                 ShowUsage();
                 return;
             }
 
-            var option = args[0];
-
-            var assemblyPath = args[1];
+            var assemblyPath = args[0];
+            var option = argsLen == 2 ? args[1] : "";
 
             if (string.IsNullOrWhiteSpace(assemblyPath))
             {
@@ -48,38 +33,29 @@ namespace AssemblyReader
 
             try
             {
-                var assembly = Assembly.LoadFrom(assemblyPath);
+                Dictionary<string, string> infos = ReadAssemblyInfos(assemblyPath);
 
-                //TODO: check these methods
-                //var assembly = Assembly.LoadFile(assemblyPath);
-
-                //var an = AssemblyName.GetAssemblyName(assemblyPath);
-                //var assembly = Assembly.Load(an);
-
-                if (assembly.FullName.Length > Program.MAX_PATH)
+                switch (option)
                 {
-                    Console.WriteLine($"Max length for namespace exceeded max value of {Program.MAX_PATH}");
-                    return;
-                }
-
-                Console.WriteLine($"Full name: {assembly.FullName}");
-
-                if (IsOptionAll(option) || IsOptionAssembly(option))
-                {
-                    string assemblyVersion = assembly.GetName().Version.ToString();
-                    Console.WriteLine($"Assembly version: {assemblyVersion}");
-                }
-
-                if (IsOptionAll(option) || IsOptionProduct(option))
-                {
-                    var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
-                    Console.WriteLine($"Product version: {versionInfo.ProductVersion}");
-                }
-
-                if (IsOptionAll(option) || IsOptionFile(option))
-                {
-                    var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
-                    Console.WriteLine($"File version: {versionInfo.FileVersion}");
+                    case "--all":
+                        foreach (var item in infos)
+                        {
+                            Console.WriteLine($"{item.Key}: {item.Value}");
+                        }
+                        break;
+                    case "--file":
+                    case "--f":
+                        Console.WriteLine($"FileVersion: {infos["FileVersion"]}");
+                        break;
+                    case "--product":
+                    case "--p":
+                        Console.WriteLine($"ProductVersion: {infos["ProductVersion"]}");
+                        break;
+                    case "--assembly":
+                    case "--a":
+                    default:
+                        Console.WriteLine($"AssemblyVersion: {infos["AssemblyVersion"]}");
+                        break;
                 }
             }
             catch (FileNotFoundException ex)
@@ -110,53 +86,47 @@ namespace AssemblyReader
 
                 Console.WriteLine(ex.Message);
             }
-            //catch (PathTooLongException ex)
-            //{
-            //    Debug.WriteLine(ex.GetType().FullName);
-            //    Debug.WriteLine(ex.Message);
+            catch (PathTooLongException ex) //TODO: maybe this can be validate
+            {
+                Debug.WriteLine(ex.GetType().FullName);
+                Debug.WriteLine(ex.Message);
 
-            //    Console.WriteLine(ex.Message);
-            //}
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static Dictionary<string, string> ReadAssemblyInfos(string assemblyPath)
+        {
+            var assembly = Assembly.LoadFrom(assemblyPath);
+            var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            var assemblyInfos = new Dictionary<string, string>
+                {
+                    { "FullName", assembly.FullName },
+                    { "TargetFramework", assembly.ImageRuntimeVersion},
+                    { "CompanyName", versionInfo.CompanyName },
+                    { "AssemblyVersion", assembly.GetName().Version.ToString() },
+                    { "ProductVersion", versionInfo.ProductVersion },
+                    { "FileVersion", versionInfo.FileVersion }
+                };
+            return assemblyInfos;
         }
 
         const int MAX_PATH = 256;
 
         private static void ShowUsage()
         {
-            Console.WriteLine("Usage: version [options] [path]");
+            Console.WriteLine("Usage: version [path] [options]");
+
+            Console.WriteLine("Path:");
+            Console.WriteLine("\t The path to the assembly. Required.");
 
             Console.WriteLine("Options:");
-            Console.WriteLine("\t --a | --assembly");
+            Console.WriteLine("\t Display different assembly information. Optional. Default is assembly version.");
+            //Console.WriteLine("\t --a | --assembly");
             Console.WriteLine("\t --p | --product");
             Console.WriteLine("\t --f | --file");
             Console.WriteLine("\t --all");
-
-            Console.WriteLine("Path:");
-            Console.WriteLine("\t The path to the assembly");
-        }
-
-        private static bool IsOptionAssembly(string option)
-        {
-            var values = new string[] { "--assembly", "--a" };
-            return Array.Exists(values, v => v == option);
-        }
-
-        private static bool IsOptionProduct(string option)
-        {
-            var values = new string[] { "--product", "--p" };
-            return Array.Exists(values, v => v == option);
-        }
-
-        private static bool IsOptionFile(string option)
-        {
-            var values = new string[] { "--file", "--f" };
-            return Array.Exists(values, v => v == option);
-        }
-
-        private static bool IsOptionAll(string option)
-        {
-            var values = new string[] { "--all" };
-            return Array.Exists(values, v => v == option);
         }
     }
 }
